225d6b0e5906fa5ab91d9b94b5940a9807fc9429
H   A   M   Z   A           E   L   J   A   R   Y <97256130+heljary@users.noreply.github.com>
2026-02-22 10:53:26 +0000

    cpp05

--- ex02/AForm.hpp
+++ ex02/AForm.hpp
@@ -28,6 +28,7 @@ class AForm{
         int getGradeIsSign() const;
         int getGradeIsExecute() const;
         int beSigned(Bureaucrat& br);
+        void execute(Bureaucrat const & executor) const = 0;
 };
 
 std::ostream& operator<<(std::ostream& out, const Form& of);
