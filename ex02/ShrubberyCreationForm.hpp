e4631191d55dac721562c8aeb0fbd5198cee6651
H   A   M   Z   A           E   L   J   A   R   Y <97256130+heljary@users.noreply.github.com>
2026-02-22 11:25:15 +0000

    cpp05

--- ex02/ShrubberyCreationForm.hpp
+++ ex02/ShrubberyCreationForm.hpp
@@ -1,11 +1,13 @@
-#ifndef "SHRUBBERYCREATIONFORM_HPP"
-#define "SHRUBBERYCREATIONFORM_HPP"
+#ifndef SHRUBBERYCREATIONFORM_HPP
+#define SHRUBBERYCREATIONFORM_HPP
+
 #include "AForm.hpp"
+#include <fstream>
 class ShrubberyCreationForm : public AForm {
 	private:
 		std::string target;
 	public:
-		
+	ShrubberyCreationForm();
 }
 
 #endif
