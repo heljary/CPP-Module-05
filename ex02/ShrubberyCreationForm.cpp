5e2a7e449471bff35d6cf2fbed7eb86978949c83
H   A   M   Z   A           E   L   J   A   R   Y <97256130+heljary@users.noreply.github.com>
2026-02-23 15:14:44 +0000

    add canonical forms implementations in ShrubberyCreationForm.cpp

---
+++ ex02/ShrubberyCreationForm.cpp
@@ -0,0 +1,27 @@
+#include "ShrubberyCreationForm.hpp"
+
+ShrubberyCreationForm::ShrubberyCreationForm() : AForm("ShrubberyCreationForm",145,137), target("default_target"){
+    std::cout << "ShrubberyCreationForm defualt constructor is called !" << std::endl;
+};
+
+
+ShrubberyCreationForm::ShrubberyCreationForm(const std::string& target): AForm("ShrubberyCreationForm",145,137), target(target){
+    std::cout << "ShrubberyCreationForm parameterized constructor is called !" std::endl;
+};
+
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& scf) : AForm(scf),target(scf.target){
+    std::cout << "ShrubberyCreationForm copy constructor is called !" << std::endl;
+}
+
+ShrubberyCreationForm& ShrubberyCreationForm::operator=(const ShrubberyCreationForm& scf){
+    if(this != &scf)
+    {
+        this->target = scf.target;
+    }
+    std::cout << "ShrubberyCreationForm operator assignment is called !" << std::endl;
+    return *this;
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm(){
+    std::cout << "ShrubberyCreationForm deconstructor is called !" << std::endl;
+}
