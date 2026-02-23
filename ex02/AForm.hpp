3f6860da8e6aad1449f8fece4a74bef7d63e15b6
H   A   M   Z   A           E   L   J   A   R   Y <97256130+heljary@users.noreply.github.com>
2026-02-23 15:13:10 +0000

    add function ft_action in Afrom

--- ex02/AForm.hpp
+++ ex02/AForm.hpp
@@ -29,7 +29,8 @@ class AForm{
         int getGradeIsSign() const;
         int getGradeIsExecute() const;
         int beSigned(Bureaucrat& br);
-        void execute(Bureaucrat const & executor) const = 0;
+        void execute(Bureaucrat const & executor) const;
+        void ft_action() = 0;
 };
 
 std::ostream& operator<<(std::ostream& out, const Form& of);
